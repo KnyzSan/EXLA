923980f1bf8d68e7cd2bc4d1cb2390c4a2e6d0a7
KnyzSan <55175359+KnyzSan@users.noreply.github.com>
2024-08-01 16:19:45 +0400

    Как я ненавижу ЭтИ ОшИбКи

--- code/PlayerMovement.cs
+++ code/PlayerMovement.cs
@@ -1,7 +1,24 @@
 using Sandbox;
+using Sandbox.Internal;
 
 public sealed class PlayerMovement : Component
 {
+	[Property] public float GroundControl = 4.0f;
+	[Property] public float AirControl = 0.1f;
+	[Property] public float MaxForce = 50f;
+	[Property] public float Speed = 150f;
+	[Property] public float RunSpeed = 310f;
+	[Property] public float CrouchSpeed = 110f;
+	[Property] public float JumpForce = 450f;
+
+	[Property] public GameObject Head{get; set;}	//getter и setter
+	[Property] public GameObject Body{get; set;}	//обязательны БЛЯТЬ!!!
+
+	public Vector3 Velocity = Vector3.Zero;
+	public bool IsCrounching = false;
+	public bool IsSprinting = false;
+
+	
 	protected override void OnUpdate()
 	{
 
